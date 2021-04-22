b1082f0a26df67584afc7e9959177639fa1a0c88
Juan Carlos Rabellino <juancarabellino@gmail.com>
2021-04-22 16:35:24 -0300

    Testing Final

--- index.js
+++ index.js
@@ -1,31 +1,36 @@
 const crud = function () {
 
-    let items = [
-        {id: 1, name: "John", lastname: "Doe"},
-        {id: 2, name: "Jane", lastname: "Doe"},
-    ]
+  let items = [
+    { id: 1, name: "John", lastname: "Doe" },
+    { id: 2, name: "Jane", lastname: "Doe" },
+  ]
 
-    let autoincrement = 3
+  let autoincrement = 3
 
-    return {
-        create: (name, lastname) => {
-            /*YOUR CODE HERE */
-
-        },
-        read: (id) => {
-            /*YOUR CODE HERE */
-
-        },
-        update: (id, name, lastname) => {
-            /*YOUR CODE HERE */
-
-        },
-        delete: (id) => {
-            /*YOUR CODE HERE */
-
-        }
+  return {
+    create: (name, lastname) => {
+      items.push({id: autoincrement, name, lastname })
+      return items[items.length-1]
+    },
+    read: (id) => {
+      let personaEncontrada = items.find(persona => persona.id === id)
+      return personaEncontrada ? personaEncontrada : null
+    },
+    update: (id, name, lastname) => {
+      let personaEncontrada = items.find(persona => persona.id === id)
+      let posicion = items.indexOf(personaEncontrada)
+      if (posicion != -1) items[posicion] = {id, name, lastname}
+      return posicion != -1 ? items[posicion] : false
+    },
+    delete: (id) => {
+      let personaEncontrada = items.find(persona => persona.id === id)
+      let posicion = items.indexOf(personaEncontrada)
+      if (personaEncontrada && posicion != -1) {
+        items.splice(posicion, 1)
+        return true
+      } else return false
     }
-
+  }
 }
 
 module.exports = crud()
